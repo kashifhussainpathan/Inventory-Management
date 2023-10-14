e484786569208cf1c290fc113802bec7e438cf4c
kashifhussainpathan <121045014+kashifhussainpathan@users.noreply.github.com>
2023-10-14 18:23:43 +0530

    added app heading

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -4,7 +4,11 @@ import { inventoryState } from "../../actions/inventory.action";
 const Home = () => {
   const inventories = inventoryState("inventories");
 
-  return <div></div>;
+  return (
+    <div className="home">
+      <h1>Inventory Management</h1>
+    </div>
+  );
 };
 
 export default Home;
